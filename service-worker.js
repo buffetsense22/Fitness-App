abb14dc048b5b9e3f3234f424d676a1b29e5ebe2
buffetsense22 <buffetsense@gmail.com>
2025-06-20 08:11:12 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('fitness-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'app.js',
+        'manifest.json',
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
